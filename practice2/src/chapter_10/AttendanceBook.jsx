8ce35cc30d300bf63be2af578bd612e31a56622e
Jhong <ghddl23@naver.com>
2024-09-01 00:02:49 +0900

    Add key in AttendanceBook.jsx

--- practice2/src/chapter_10/AttendanceBook.jsx
+++ practice2/src/chapter_10/AttendanceBook.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const student = [
     {
+        id : 1,
         name: 'jhong',
     },
     {
+        id : 2,
         name: 'hard to naming',
     },
     {
+        id : 3,
         name: 'react',
     },
     {
+        id : 4,
         name: 'hello,world'
     },
 ];
@@ -19,7 +23,7 @@ function AttendanceBook(props){
     return(
         <ul>
             {student.map((student) =>{
-                return <li>{student.name}</li>;
+                return <li key={student.id}>{student.name}</li>;
             })}
         </ul>
     );
