20084c24daa7bc97105ae84f655d05b0ededf1f3
Jhong <ghddl23@naver.com>
2024-09-01 19:12:58 +0900

    Add chapter_13

---
+++ practice2/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props){
+    return(
+        <Card title="jhong" backgroundColor="#F0F0F0">
+            <p>hello, world!</p>
+            <p>i'm jhong</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
